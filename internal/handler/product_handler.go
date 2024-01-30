47f6a3ad0324df9de1235540bc179e803a6c3f66
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2024-01-30 18:25:05 -0300

    feat: create product handler

---
+++ internal/handler/product_handler.go
@@ -0,0 +1,77 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/jfilipedias/ecommerce-fullcycle/api/internal/entity"
+	"github.com/jfilipedias/ecommerce-fullcycle/api/internal/service"
+)
+
+type ProductHandler struct {
+	ProductService service.ProductService
+}
+
+func NewProductHandler(productService *service.ProductService) *ProductHandler {
+	return &ProductHandler{ProductService: *productService}
+}
+
+func (ph *ProductHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var product entity.Product
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	result, err := ph.ProductService.Create(product.Name, product.Description, product.Price, product.CategoryID, product.ImageURL)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(result)
+}
+
+func (ph *ProductHandler) GetAll(w http.ResponseWriter, r *http.Request) {
+	products, err := ph.ProductService.GetAll()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(products)
+}
+
+func (ph *ProductHandler) GetByID(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		http.Error(w, "id is required", http.StatusBadRequest)
+		return
+	}
+
+	product, err := ph.ProductService.GetByID(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(product)
+}
+
+func (ph *ProductHandler) GetByCategoryID(w http.ResponseWriter, r *http.Request) {
+	categoryID := chi.URLParam(r, "categoryID")
+	if categoryID != "" {
+		http.Error(w, "categoryID is required", http.StatusBadRequest)
+		return
+	}
+
+	products, err := ph.ProductService.GetByCategoryID(categoryID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(products)
+}
