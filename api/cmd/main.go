7117fefe62faa1e2b943b66ab6b33f7a03520c11
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2024-01-30 19:50:58 -0300

    feat: setup router

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jfilipedias/ecommerce-fullcycle/api/internal/handler"
+	"github.com/jfilipedias/ecommerce-fullcycle/api/internal/repository"
+	"github.com/jfilipedias/ecommerce-fullcycle/api/internal/service"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
@@ -19,4 +26,30 @@ func main() {
 		panic(err.Error())
 	}
 	defer db.Close()
+
+	categoryRepository := repository.NewCategoryRepository(db)
+	categoryService := service.NewCategoryService(*categoryRepository)
+	categoryHandler := handler.NewCategoryHandler(categoryService)
+
+	productRepository := repository.NewProductRepository(db)
+	productService := service.NewProductService(*productRepository)
+	productHandler := handler.NewProductHandler(productService)
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Post("/categories", categoryHandler.Create)
+	r.Get("/categories", categoryHandler.GetAll)
+	r.Get("/categories/{id}", categoryHandler.GetByID)
+
+	r.Post("/products", productHandler.Create)
+	r.Get("/products", productHandler.GetAll)
+	r.Get("/products/{id}", productHandler.GetByID)
+	r.Get("/products/categories/{categoryID}", productHandler.GetByCategoryID)
+
+	p := os.Getenv("PORT")
+	fmt.Printf("Server is running on port %q", p)
+	http.ListenAndServe(p, r)
 }
