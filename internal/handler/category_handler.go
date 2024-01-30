5e3efa32d82e9459bed10550dbe330354df36a0c
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2024-01-30 15:16:35 -0300

    feat: create category handler

---
+++ internal/handler/category_handler.go
@@ -0,0 +1,60 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/jfilipedias/ecommerce-fullcycle/api/internal/entity"
+	"github.com/jfilipedias/ecommerce-fullcycle/api/internal/service"
+)
+
+type CategoryHandler struct {
+	CategoryService *service.CategoryService
+}
+
+func NewCategoryHandler(categoryService *service.CategoryService) *CategoryHandler {
+	return &CategoryHandler{CategoryService: categoryService}
+}
+
+func (ch *CategoryHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var category entity.Category
+	err := json.NewDecoder(r.Body).Decode(&category)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	result, err := ch.CategoryService.Create(category.Name)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(result)
+}
+
+func (ch *CategoryHandler) GetAll(w http.ResponseWriter, r *http.Request) {
+	categories, err := ch.CategoryService.GetAll()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(categories)
+}
+
+func (ch *CategoryHandler) GetByID(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		http.Error(w, "id is required", http.StatusBadRequest)
+	}
+
+	category, err := ch.CategoryService.GetByID(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(category)
+}
